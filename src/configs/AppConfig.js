328c7a9689d05fc55c4a4358c675d9c9a3724e9a
Judex66 <sermac2000@mail.ru>
2025-03-24 09:55:48 +0300

    fix deploy

--- src/configs/AppConfig.js
+++ src/configs/AppConfig.js
@@ -3,8 +3,8 @@ import { env } from './EnvironmentConfig'
 
 export const APP_NAME = 'Emilus';
 export const API_BASE_URL = env.API_ENDPOINT_URL
-export const APP_PREFIX_PATH = '/app';
-export const AUTH_PREFIX_PATH = '/auth';
+export const APP_PREFIX_PATH = '/test_project/app';
+export const AUTH_PREFIX_PATH = '/test_project/auth';
 
 export const THEME_CONFIG = {
 	navCollapsed: false,
